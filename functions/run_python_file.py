a376ed252fa0ac7ada8249d1a8ede8ee2c5ce59e
David <dclaydon@gmail.com>
2025-06-17 14:57:34 +0100

    fixed error message

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -7,7 +7,7 @@ def run_python_file(working_directory, file_path):
     abs_file_path = os.path.abspath(str(os.path.join(working_directory, file_path)))
 
     if not abs_file_path.startswith(abs_working_dir, 0, len(abs_working_dir)):
-        return f"Error: Cannot execute '{file_path}' as it is outside the permitted working directory"
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
 
     if not os.path.isfile(abs_file_path):
         return f'Error: File "{file_path}" not found.'
