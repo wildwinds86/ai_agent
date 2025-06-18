4080107bc56b57e53a6f60d79f5490de10d7448f
david <dclaydon@gmail.com>
2025-06-18 14:10:01 +0100

    ...

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from functions.run_python_file import  run_python_file
 
 if __name__ == "__main__":
-    print(run_python_file("calculator", "main.py"))
-    print(run_python_file("calculator", "tests.py"))
-    print(run_python_file("calculator", "../main.py"))
-    print(run_python_file("calculator", "nonexistent.py"))
+	print(run_python_file("calculator", "main.py"))
+	print(run_python_file("calculator", "tests.py"))
+	print(run_python_file("calculator", "../main.py"))
+	print(run_python_file("calculator", "nonexistent.py"))
