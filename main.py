16491dd994d127708153dd51d76fd6bfe9f7e11b
David <dclaydon@gmail.com>
2025-06-18 13:46:54 +0100

    added system prompt to main.py

--- main.py
+++ main.py
@@ -31,9 +31,11 @@ def main():
 
 
 def generate_content(client, messages, verbose):
+    system_prompt = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT"
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
