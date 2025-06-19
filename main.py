1ed6873512540e335fdb57d40bab979388dab9a0
david <dclaydon@gmail.com>
2025-06-19 09:20:32 +0100

    ...

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import os
 import sys
-
-from dotenv import load_dotenv
+import os
 from google import genai
 from google.genai import types
+from dotenv import load_dotenv
+
+from prompts import system_prompt
+from call_function import available_functions
 
 
 def main():
@@ -15,7 +17,7 @@ def main():
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here" [--verbose]')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
 
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -26,60 +28,30 @@ def main():
     if verbose:
         print(f"User prompt: {user_prompt}\n")
 
-    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     generate_content(client, messages, verbose)
 
 
 def generate_content(client, messages, verbose):
-    system_prompt = system_prompt = """
-You are a helpful AI coding agent.
-
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-- List files and directories
-
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
-
-    schema_get_files_info = types.FunctionDeclaration(
-        name="get_files_info",
-        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
-        parameters=types.Schema(
-            type=types.Type.OBJECT,
-            properties={
-                "directory": types.Schema(
-                    type=types.Type.STRING,
-                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
-                ),
-            },
-        ),
-    )
-
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info,
-        ]
-    )
-
-    config = types.GenerateContentConfig(
-        tools=[available_functions], system_instruction=system_prompt
-    )
-
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=config,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    if response.function_calls is not None:
-        func_calls = response.function_calls
+    if not response.function_calls:
+        return response.text
 
-    print("Response:")
-    print(response.text)
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":
